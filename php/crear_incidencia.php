36385711ec41dfe39fe382fd0f889db4d793c1e9
mantonioinspe <a23marmargue@inspedralbes.cat>
2026-04-27 13:41:04 +0200

    Declaracio de variables i modificacio form crear incidencia

--- php/crear_incidencia.php
+++ php/crear_incidencia.php
@@ -12,12 +12,13 @@ require_once 'connexio.php';
 function crear_incidencia($conn)
 {
     // Obtenir el nom de la casa del formulari
-    $nom = $_POST['nom'];
+    $id_departament = $_POST['id_departament'];
     $id_tipo = $_POST['id_tipo'];
     $data_fin = $_POST['data_fin'];
     $data_inici = $_POST['id_tecnic'];
     $prioridad = $_POST['prioridad'];
     $descripcio = $_POST['descripcio'];
+    $id_tecnic = $_POST['id_tecnic'];
 
     // Comprovar si el nom no està buit
     // Si l'html està ben escrit això no podria passar en els usuaris normals
@@ -29,9 +30,9 @@ function crear_incidencia($conn)
     }
 
     // Preparar la consulta SQL per inserir una nova casa
-    $sql = "INSERT INTO incidencia (data_inici, id_tipo, data_fin, id_tecnic, prioridad, descripcio) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO incidencia (id_departament, id_tipo, data_fin, data_inici, prioridad, descripcio, id_tecnic) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);  //La variable $conn la tenim per haver inclòs el fitxer connexio.php
-    $stmt->bind_param("ssssss", $nom , $id_tipo, $data_fin, $data_inici, $prioridad, $descripcio);
+    $stmt->bind_param("iididissi", $id_departament ,$id_tipo, $data_fin, $data_inici, $prioridad, $descripcio, $id_tecnic);
 
     // Executar la consulta i comprovar si s'ha inserit correctament
     if ($stmt->execute()) {
@@ -52,7 +53,7 @@ function crear_incidencia($conn)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear</title>
+    <title>Crear incidencia</title>
 </head>
 
 <body>
