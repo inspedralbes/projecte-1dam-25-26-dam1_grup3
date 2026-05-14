2e2614d26f238ef442062f7e892c56f2936fe212
a25alarosure <a25alarosure@inspedralbes.cat>
2026-05-14 09:39:10 +0200

    canvi de css y modificacio en l'estil visual de botons del index.php

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GI3P — Inici</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/estils.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/estils.css">
     <link rel="icon" type="image/jpg" href="img/icon.jpg">
 </head>
 <body>
